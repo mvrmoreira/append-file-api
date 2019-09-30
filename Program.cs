2d4cc7e20dbcdc871bebac135e9eeb22af53a182
Matheus Moreira | Stone <mvrmoreira@mamute.local>
2019-09-30 15:52:24 -0300

    Change port to 22

--- Program.cs
+++ Program.cs
@@ -19,6 +19,8 @@ namespace append_file_api
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseUrls("http://localhost:5022")
+                ;
     }
 }
